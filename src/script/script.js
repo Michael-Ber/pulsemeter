821eb52f926ba78863399e0e0bae8fd1557c37ac
Michael <example@gmail.com>
2020-07-21 12:20:49 +0800

    jquery validate forms

--- src/script/script.js
+++ src/script/script.js
@@ -42,12 +42,49 @@
 //slick slider
 
 $(document).ready(function(){
+
     $('.slider__inner').slick({
         slidesToShow: 1,
         nextArrow: '<button type="button" class="slick-prev"><img src="img/icons/right.png" alt="right" id="right"></button>',
         prevArrow: '<button type="button" class="slick-next"><img src="img/icons/left.png" alt="left" id="left"></button>',
         speed: 1000,
         mobileFirst: true,
+	});
+	
+	function validateForm(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 3,
+					maxlength: 10
+				},
+				tel: {
+					required: true,
+				},
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста введите имя",
+					minlength: jQuery.validator.format("Введите не менее {0} символов"),
+					maxlength: jQuery.validator.format("Введите не более {0} символов")
+				},
+				tel: "Необходимо ввести номер телефона",
+				email: {
+					required: "Необходимо ввести почту",
+					email: "Неправильно введен адрес почты"
+				}
+			  }
+		});
+	}
 
-  });
+	validateForm('#consultation-form');
+	validateForm('#consultation form');
+	validateForm('#order form');
+
+	$('input[name=tel]').mask("+7 (999) 999-9999",{placeholder:"_"});
 });
